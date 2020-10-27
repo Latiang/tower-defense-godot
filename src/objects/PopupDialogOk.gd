07d32ba10f63e4ee570e954fafd2269da6396368
wsandst <williamsandstrom99@hotmail.com>
2020-10-27 12:45:56 +0100

    Add a Popup node for various messages

---
+++ src/objects/PopupDialogOk.gd
@@ -0,0 +1,22 @@
+tool
+extends PanelContainer
+
+signal button_pressed()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var text = ""
+export var button_text = ""
+export var close_on_press = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/MarginContainer2/Label.text = text
+	$MarginContainer/VBoxContainer/MarginContainer/Button.text = button_text
+
+func _on_Button_pressed():
+	emit_signal("button_pressed")
+	if close_on_press:
+		visible = false
---
+++ src/objects/PopupDialogTwoChoices.gd
@@ -0,0 +1,31 @@
+tool
+extends PanelContainer
+
+signal button1_pressed()
+signal button2_pressed()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var text = ""
+export var button1_text = ""
+export var button2_text = ""
+export var close_on_press = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$MarginContainer/VBoxContainer/MarginContainer2/Label.text = text
+	$MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/Button1.text = button1_text
+	$MarginContainer/VBoxContainer/MarginContainer/HBoxContainer/Button2.text = button2_text
+
+func _on_Button1_pressed():
+	emit_signal("button1_pressed")
+	if close_on_press:
+		visible = false
+
+
+func _on_Button2_pressed():
+	emit_signal("button2_pressed")
+	if close_on_press:
+		visible = false
